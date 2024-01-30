937ff088b4a02540ebdfd1d9d1e9ae6029b8562d
pnickchi <payman.nickchi@gmail.com>
2024-01-30 13:00:59 -0800

    Initial upload of Gamma example version

--- scripts/stoch_grad_OAIS_Gamma.jl
+++ scripts/stoch_grad_OAIS_Gamma.jl
@@ -1,76 +1,81 @@
 #
 # Implementation of stochastic gradiant optimised adaptive importance sampling (normalised case)
 # Description:
-# This script implements the normalised case of  stochastic gradiant optimised adaptive importance 
+# This script implements the normalised case of stochastic gradiant optimised adaptive importance 
 # sampling method based on the paper: 
 # "Convergence rates for optimised adaptive importance samplers" by Omer Deniz Akyildiz
 # and Joaquin Miguez in Statistics and Computing Journal. 
 #
 # Details: Algorithm 2, Stochastic gradient OAIS on page 7 of 17 of the paper. 
 #
-# I used the following example in this script: 
-# target:    f(x) = ( 1/sqrt(2*pi) ) exp( -0.5 x^2 )        X ~ N(0,1)
-# proposal:  g(x) = ( 1/sqrt(2*pi) ) exp( -0.5 (x-\mu)^2 )  X ~ N(\mu,1)
+# For this example, we work on the following example in this script: 
+# target:    f(x) X ~ Gamma(\alpha,\lambda)
+# proposal:  g(x) X ~ Exponential(\lambda)
 #
 
 
 using Distributions
 using Statistics
-
+using SpecialFunctions
+using Plots
 
 
 #
-# Define the target distribution, f(x) standard Normal distribution.
-# The function Takes x and returns p.d.f of N(0,1)
+# Define the target distribution, f(x) Gamma distribution.
+# The function Takes x and returns p.d.f of Gamma(\alpha,\lambda)
 # 
-function f(x)
-    ( 1/sqrt(2*pi) ) .* exp.( -0.5*(x).^2 )
+function f(x, alpha, lambda)
+    (lambda^(alpha))/(gamma(alpha)) * x.^(alpha-1) .* exp.(-lambda * x)
 end
 
 
 #
-# Define the proposal function, g(x,mu) N(\mu,1) 
-# A function of x (sample) and mu parameter (mean)
-# For any values of x and \mu, returns the p.d.f of N(\mu,1)
+# Define the proposal function, g(x,lambda) Exponential(\lambda)
+# A function of x (sample) and lambda parameter
+# For any values of x and \lambda, it returns the p.d.f of Exponential(\lambda)
 #
-function g(x, mu)
-    (1/sqrt(2*pi)) .* exp.(-0.5 * (x.-mu).^2 )
+function g(x, lambda)
+    lambda * exp.(-lambda * x)
  end
 
 
  #
  # Analytical gradient of the effective sample size.
  #
- function G(mu)
-    2*mu*exp(mu^2)
+ function compute_exact_gradient(alpha, lambda)
+    (1+ (2*alpha - 1)/(lambda) ) * (-gamma(2*alpha-1)/lambda*gamma(alpha)*gamma(alpha))
  end
 
 #
 # Define a function to update the proposal at each iteration, let's call this function update_proposal.
-# This function takes the sample (x) and parameter (mu) at each iteration and returns
-# a new value for the parameter (mu). 
+# This function takes the sample (x) and parameters (\alpha and \lambda) at each iteration and returns
+# a new value for the parameters. 
 #
 # (Some notes on stepsize: 
 # the best practical suggestion is to set stepsize as \frac{1}{t} where t is the iteration number.
 # Usually the range for stepsize is between \frac{1}{sqrt(t)} and \frac{1}{t}
 # Anything in this ranges should work it is a matter of time for convergence.)
 # 
-function update_proposal(x, mu, stepsize)
+function update_proposal(x, alpha, lambda, stepsize)
 
-   num = f.(x).^2
-   den = g.(x,mu).^2
-   temp = (mu .- x) .* (num ./ den)
+   num = f.(x,alpha,lambda).^2
+   den = g.(x,lambda).^2
+   temp = -( (1/lambda) .+ x) .* (num ./ den)
    gt = mean(temp)
-   new_mu = mu - gt*stepsize
-   return new_mu
+   theta = [alpha,lambda]
+   new_theta = theta .- gt*stepsize
+   return new_theta
 
 end
 
 
-# Update the proposal using our analytical expression for the gradient of the effective sample size. Note that this analytical update does not require any Monte Carlo samples.
-function update_proposal_analytical(mu, stepsize)
-    new_mu = mu - G(mu)*stepsize
-    return new_mu
+#
+# Update the proposal using our analytical expression for the gradient of the effective sample size. 
+# Note that this analytical update does not require any Monte Carlo samples.
+# 
+function update_proposal_analytical(theta, stepsize)
+    new_theta = theta .- compute_exact_gradient(theta[1],theta[2])*stepsize
+    return new_theta
 end
 
 #
@@ -78,8 +83,8 @@ end
 # The inputs are sample (x) and parameter (mu) at each iteration.
 # The ouput is a vector of weights for each observation in x.
 #
-function compute_weights(x, mu, normalize=true)
-    weights = f.(x) ./ g.(x,mu)
+function compute_weights(x, alpha, lambda, normalize = true)
+    weights = f.(x,alpha,lambda) ./ g.(x,lambda)
     if normalize
         weights = weights ./ mean(weights)
     end
@@ -87,8 +92,8 @@ function compute_weights(x, mu, normalize=true)
 end
 
 
-function sample_from_proposal(n,mu)
-    u = rand(Normal(mu,1),n)
+function sample_from_proposal(n,lambda)
+    u = rand(Exponential(lambda),n)
     return u
 end
 
@@ -97,9 +102,9 @@ end
 # Initialize values
 #
 
-# Initial value for mu in proposal
-mu = 1
-# let mu = 1
+# Initial value for alpha and lambda in proposal distribution
+alpha = 1
+lambda = 1
 
 # Number of Monte Carlo samples
 T = 100
@@ -107,40 +112,13 @@ T = 100
 # Number of random sample at each iteration
 N = 1000
 
-xx = sample_from_proposal(N,mu)
+xx = sample_from_proposal(N,lambda)
 for t in 1:T
-   # You can change stepsize if you run into problem 
-   # but it should be ok with this one (William: I think so).
-    mu = update_proposal(xx,mu, 0.2*(1/t)^(0.55) ) 
-    xx = sample_from_proposal(N,mu)
-    ww = compute_weights(xx,mu)
-    println(mu)
-    #println(sum(ww .* xx))
+    theta = update_proposal_analytical( [alpha,lambda], 0.2*(1/t)^(0.55) ) 
+    xx    = sample_from_proposal(N,theta[2])
+    ww    = compute_weights(xx,theta[1],theta[2])
+    println(theta)
+    println(mean(ww .* xx))
 end
-# end
 
-
-
-ww = compute_weights(xx,mu, false)
-var(ww)
-
-ww_norm = compute_weights(xx,mu)
-var(ww_norm)
-
-
-
-mu = 1
-for t in 1:T
-    # You can change stepsize if you run into problem 
-    # but it should be ok with this one (William: I think so).
-     mu = update_proposal_analytical(mu,0.3*(1/t)^(0.7) )
-     println(mu)
-     #println(sum(ww .* xx))
- end
-
-
-
- #
- # Example 2: Target distribution is Gamma(\alpha,\lambda) and proposal is Exp(\lambda)
- #
- 
+histogram(xx)
