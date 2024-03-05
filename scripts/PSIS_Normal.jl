1760c59b3179a4e9259828b936f090dce1aeb3c7
William Ruth <wruth1@users.noreply.github.com>
2024-03-04 16:20:43 -0800

    Add rho to plot of k-hat in normal example

--- scripts/PSIS_Normal.jl
+++ scripts/PSIS_Normal.jl
@@ -44,6 +44,15 @@ function g(x, mu)
     (1/sqrt(2*pi)) .* exp.(-0.5 * (x.-mu).^2 )
  end
 
+ #
+ # Analytical gradient of the effective sample size.
+ #
+ function G(mu)
+    2*mu*exp(mu^2)
+ end
+
+
+
 #
 # Define a function to compute weights at each iteration.
 # The inputs are sample (x) and parameter (mu) at each iteration.
@@ -64,12 +73,30 @@ function sample_from_proposal(n,mu)
 end
 
 
+"""
+Analytical value of the objective function we are trying to minimize.
+"""
+function rho(mu)
+    exp(mu^2)
+end
+
+"""
+Estimate of our objective function obtained from an importance sample.
+"""
+function rho_hat(all_Xs, mu)
+    all_wts = compute_weights(all_Xs, mu, false)
+    return mean(all_wts .^2)
+end
+
+
 #
 # Initialize values
 #
 
 # List of mu values to try
-mu_vec = collect(-5:0.1:5)
+## Note: rho grows like exp(mu^2), so you really have to zoom-in to see what's happening near zero
+# mu_vec = collect(-3:0.1:3)
+mu_vec = collect(-2:0.1:2)
 
 
 # Size of each sample
@@ -79,7 +106,7 @@ N = 1000
 M = 500
 
 all_k_hats = zeros(length(mu_vec), M)
-
+all_rho_hats = zeros(length(mu_vec), M)
 
 using Suppressor
 
@@ -89,13 +116,18 @@ using Suppressor
         ww = compute_weights(xx,mu, false)
         _, k_hat = pareto_smooth(ww, M_keep="default", return_k=true)
         all_k_hats[i,m] = k_hat
+
+        all_rho_hats[i,m] = rho_hat(xx, mu)
     end
 end
+
+all_true_rhos = rho.(mu_vec)
 
 
 
 # Plot the mean k_hat for each value of mu
-plot(mu_vec, mean(all_k_hats, dims=2), xlabel=L"\mu", ylabel=L"\hat{k}", label=L"mean $\hat{k}$", title=L"Mean $\hat{k}$ for each value of $\mu$")
+## Use a hacky solution to use a single legend. I will add this label later.
+plot(mu_vec, mean(all_k_hats, dims=2), xlabel=L"\mu", ylabel=L"\hat{k}", label=nothing, title=L"Mean $\hat{k}$ for each value of $\mu$")
 # Add a horizontal line at 0.7
 hline!([0.7], label=nothing, color=:red, linestyle=:dash)
 
@@ -103,6 +135,29 @@ hline!([0.7], label=nothing, color=:red, linestyle=:dash)
 ucls = mean(all_k_hats, dims=2) .+ 1.96*std(all_k_hats, dims=2) / sqrt(M);
 lcls = mean(all_k_hats, dims=2) .- 1.96*std(all_k_hats, dims=2) / sqrt(M);
 
-plot!(mu_vec, ucls, label="95% CI", color=:black, linestyle=:dash)
-plot!(mu_vec, lcls, label=nothing, color=:black, linestyle=:dash)
+#* Note: I remove the confidence bands because having two sets of them makes the plot too messy.
+# plot!(mu_vec, ucls, label=nothing, color=:black, linestyle=:dashdot)
+# plot!(mu_vec, lcls, label=nothing, color=:black, linestyle=:dashdot)
+
+
+
+# Add estimated and true values of rho. Use the right vertical axis.
+rhs = twinx()
+plot!(rhs, mu_vec, all_true_rhos, ylabel=L"\rho(\mu)", label=L"\rho", color=:green, linestyle=:dash)
+
+# Add mean estimated values of rho. Use the right vertical axis.
+plot!(rhs, mu_vec, mean(all_rho_hats, dims=2), label=L"\hat{\rho}", color=:purple, linestyle=:dash)
+
+# Add confidence bands
+ucls_rho = mean(all_rho_hats, dims=2) .+ 1.96*std(all_rho_hats, dims=2) / sqrt(M);
+lcls_rho = mean(all_rho_hats, dims=2) .- 1.96*std(all_rho_hats, dims=2) / sqrt(M);
+
+#* Note: I remove the confidence bands because having two sets of them makes the plot too messy.
+# plot!(rhs, mu_vec, ucls_rho, label=nothing, color=:black, linestyle=:dashdot)
+# plot!(rhs, mu_vec, lcls_rho, label=nothing, color=:black, linestyle=:dashdot)
+
+
+# Add legend for original plot
+plot!(rhs, mu_vec, NaN.*mean(all_k_hats, dims=2), xlabel=L"\mu", ylabel=L"\hat{k}", label=L"mean $\hat{k}$", title=L"Mean $\hat{k}$ for each value of $\mu$")
+
 
