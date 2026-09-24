b3302d4c88903da218969e57d2cf5619c4924966
William <wruth@mtroyal.ca>
2026-09-24 14:50:37 -0600

    Automate main workflow

--- scripts/SA on Different ESS/Z from N(0,sigma).r
+++ scripts/SA on Different ESS/Z from N(0,sigma).r
@@ -1,4 +1,5 @@
 
+pacman::p_load(tidyr)
 
 
 #
@@ -6,7 +7,7 @@
 # The function Takes x and returns p.d.f of N(0,1)
 #
 f = function(x){
-  ( 1/sqrt(2*pi) ) * exp( -0.5*(x)^2 )
+  dnorm(x)
 }
 
 
@@ -26,6 +27,8 @@ sim_G = function(N, sigma){
 sim_G_from_U = function(U, sigma){
   qnorm(U, 0, sigma)
 }
+
+
 
 get_L1_ESS = function(W){
     N = length(W)
@@ -143,44 +146,109 @@ sigma_max = 2.5
 # flavour = "L1"
 # flavour = "L2"
 # flavour = "LInfty"
-flavour = "entropy"
+# flavour = "entropy"
 
 
-set.seed(111)
 
-# Initial value for sigma in proposal
-sigma = numeric(MC)
-sigma[1] = 2
 
-for(i in 2:MC){
-  print(paste0(i, " out of ", MC))
-  # sigma[i]  = update_proposal(sigma[i-1], N, get_a(i, alpha = -1, a0 = 10), get_c(i), flavour = flavour)
-    sigma_new = update_proposal(sigma[i-1], N, get_a(i, a0 = 0.01), get_c(i), flavour = flavour)
-    if(sigma_new < sigma_min){
-        sigma[i] = sigma_min
-    } else if(sigma_new > sigma_max){
-        sigma[i] = sigma_max
-    } else{
-        sigma[i] = sigma_new
+
+
+#
+# sigma_0: Initial value
+
+run_SA = function(theta_0, flavour,
+                    theta_min = -Inf, theta_max = Inf,
+                    N = 1000, MC = 500, 
+                    a0 = 1, c0 = 0.001, 
+                    alpha_a = -0.501, alpha_c = -0.501,
+                    verbose = FALSE){
+
+  # Initial value for theta in proposal
+  theta = numeric(MC)
+  theta[1] = theta_0
+
+
+  for(i in 2:MC){
+    if(verbose){
+      if((i %% 50) == 0)  print(paste0(i, " out of ", MC))
+      # print(paste0(i, " out of ", MC))
     }
+      
+      # Run update
+      # theta[i]  = update_proposal(theta[i-1], N, get_a(i, alpha = -1, a0 = 10), get_c(i), flavour = flavour)
+      theta_new = update_proposal(theta[i-1], N, get_a(i, alpha = alpha_a, a0 = a0), get_c(i, alpha = alpha_c, c0 = c0), flavour = flavour)
+
+      # Check bounds
+      if(theta_new < theta_min){
+          theta[i] = theta_min
+      } else if(theta_new > theta_max){
+          theta[i] = theta_max
+      } else{
+          theta[i] = theta_new
+      }
+  }
+
+  return(theta)
 }
 
-print(sigma)
 
 
-# sigma_L1 = sigma
-# sigma_L2 = sigma
-# sigma_LInfty = sigma
-# sigma_entropy = sigma
 
 
-pacman::p_load(tidyr)
+make_trajectories = function(MC, LInfty_a0, LInfty_alpha_a){
 
-data_sigma = tibble(i = 1:MC, L1 = sigma_L1, L2 = sigma_L2, Infty = sigma_LInfty, entropy = sigma_entropy) %>%
-    pivot_longer(2:5, names_to = "flavour", values_to = "sigma")
+  set.seed(1111)
+
+  theta_L1 = run_SA(theta_0 = 2, flavour = "L1",
+                      theta_min = 0.1, theta_max = 2.5,
+                      N = 1000, MC = MC, 
+                      a0 = 0.001, c0 = 0.001, 
+                      alpha_a = -1, alpha_c = -0.501)
+
+  set.seed(2222)
+
+  theta_L2 = run_SA(theta_0 = 2, flavour = "L2",
+                      theta_min = 0.1, theta_max = 2.5,
+                      N = 1000, MC = MC, 
+                      a0 = 0.01, c0 = 0.001, 
+                      alpha_a = -0.501, alpha_c = -0.501)
+
+  set.seed(3333)
+
+  # theta_LInfty = run_SA(theta_0 = 2, flavour = "LInfty",
+  #                     theta_min = 0.1, theta_max = 2.5,
+  #                     N = 1000, MC = MC, 
+  #                     a0 = 0.001, c0 = 0.001, 
+  #                     alpha_a = -1, alpha_c = -0.501)
+
+  theta_LInfty = run_SA(theta_0 = 2, flavour = "LInfty",
+                      theta_min = 0.1, theta_max = 2.5,
+                      N = 1000, MC = MC, 
+                      a0 = LInfty_a0, c0 = 0.001, 
+                      alpha_a = LInfty_alpha_a, alpha_c = -0.501)            
 
 
-ggplot(data_sigma, aes(x = i, y = sigma)) + geom_line() + facet_wrap(~flavour)
+  set.seed(4444)
+
+  theta_entropy = run_SA(theta_0 = 2, flavour = "entropy",
+                      theta_min = 0.1, theta_max = 2.5,
+                      N = 1000, MC = MC, 
+                      a0 = 0.01, c0 = 0.001, 
+                      alpha_a = -0.501, alpha_c = -0.501)
+
+
+
+  data_theta = tibble(i = 1:MC, L1 = theta_L1, L2 = theta_L2, Infty = theta_LInfty, entropy = theta_entropy) %>%
+      pivot_longer(2:5, names_to = "flavour", values_to = "theta")
+
+    return(data_theta)
+}
+
+data_theta = make_trajectories(MC, LInfty_a0 = 0.0005, LInfty_alpha_a = -1)
+
+ggplot(data_theta, aes(x = i, y = theta)) + geom_line() + facet_wrap(~flavour)
+
+
 
 
 # ToDo: Make a plot of the true ESS as a function of sigma. Compute the true value by Monte Carlo with high precision
@@ -197,3 +265,6 @@ data_sigma_ave = tibble(i = 1:MC, L1 = sigma_ave_L1, L2 = sigma_ave_L2, Infty = 
     pivot_longer(2:5, names_to = "flavour", values_to = "sigma_ave")
 
 ggplot(data_sigma_ave, aes(x = i, y = sigma_ave)) + geom_line() + facet_wrap(~flavour) + geom_hline(yintercept = 1)
+
+
+
